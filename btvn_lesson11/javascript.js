73e5a5e596df890d1478395fff237a55da03b591
kngan2000 <91657071+kngan2000@users.noreply.github.com>
2022-12-13 22:13:23 +0700

    test

---
+++ btvn_lesson11/javascript.js
@@ -0,0 +1,4 @@
+// Write a program to create a traffic light.
+// Note: When you click on red button, the red light is displayed
+// When you click on  yellow button, the yellow light is displayed
+// When you click on  green button, the green light is displayed
